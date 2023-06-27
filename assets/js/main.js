e778e198a0945389aaf3e7b8a2b14ec46b12157d
Yago <yago.costa1@ibm.com>
2023-06-27 14:52:23 -0300

    Colocando a parte de professionalEXP

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,6 +52,20 @@ function updatePortfolio(profileData) {
     }).join('')
 }
 
+function updateProfessionalExperience(profileData) {
+    const professionalExperience = document.getElementById('profile.professionalExperience')
+
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+        return `
+                <li>
+                    <h3 class="title">${experience.name}</h3>
+                    <p class="period">${experience.period}</p>
+                    <p>${experience.description}</p>
+                </li>
+        `
+    }).join('')
+}
+
 (async () => {
 
     const profileData = await fetchProfileData()
@@ -60,4 +74,5 @@ function updatePortfolio(profileData) {
     updatehardSkills(profileData)
     updateLanguages(profileData)
     updatePortfolio(profileData)
+    updateProfessionalExperience(profileData)
 })()
